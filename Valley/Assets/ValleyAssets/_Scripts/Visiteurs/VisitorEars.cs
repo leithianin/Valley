ccaf69fcad373d61a155f06fd3ad2f2fb983b289
Timothe <linkazer@gmail.com>
2021-12-03 16:14:31 +0100

    Setmap for Test

--- Valley/Assets/ValleyAssets/_Scripts/Visiteurs/VisitorEars.cs
+++ Valley/Assets/ValleyAssets/_Scripts/Visiteurs/VisitorEars.cs
@@ -9,8 +9,10 @@ public class VisitorEars : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         VisitorAgentBehave otherVisitor = other.GetComponent<VisitorAgentBehave>(); // Voir pour ne pas utiliser de GetComponent
+        Debug.Log("TestTrigger");
         if (otherVisitor != null && otherVisitor != visitor)
         {
+            Debug.Log("Detect other visitor");
             visitor.CrossVisitor();
         }
     }
