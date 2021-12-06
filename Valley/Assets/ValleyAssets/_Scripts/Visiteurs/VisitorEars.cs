1f2f9f80ad6c9b1ec6c5a48fcfd1242f6187d284
Timothe <linkazer@gmail.com>
2021-12-06 10:52:52 +0100

    Remove some Debugs

--- Valley/Assets/ValleyAssets/_Scripts/Visiteurs/VisitorEars.cs
+++ Valley/Assets/ValleyAssets/_Scripts/Visiteurs/VisitorEars.cs
@@ -9,10 +9,8 @@ public class VisitorEars : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         VisitorAgentBehave otherVisitor = other.GetComponent<VisitorAgentBehave>(); // Voir pour ne pas utiliser de GetComponent
-        Debug.Log("TestTrigger");
         if (otherVisitor != null && otherVisitor != visitor)
         {
-            Debug.Log("Detect other visitor");
             visitor.CrossVisitor();
         }
     }
