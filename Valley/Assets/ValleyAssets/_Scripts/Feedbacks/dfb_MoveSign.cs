fa716f8fd38317741789e75306d25fa7c91d3aa4
Timothe <linkazer@gmail.com>
2021-12-08 16:26:17 +0100

    Tool bounce done

--- Valley/Assets/ValleyAssets/_Scripts/Feedbacks/dfb_MoveSign.cs
+++ Valley/Assets/ValleyAssets/_Scripts/Feedbacks/dfb_MoveSign.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,8 @@ public class dfb_MoveSign : MonoBehaviour
 
     private bool isActive = true;
 
+    [SerializeField] private AnimationCurve curve;
+
     public void Start()
     {
         initialPos = transform.position;
@@ -24,21 +27,53 @@ public class dfb_MoveSign : MonoBehaviour
         {
             UIManager.GetOnMovingTool?.Invoke();
             Vector3 moveVector = move.position;
-            rt.DOMove(moveVector, timeFeedback).OnComplete(() =>
+
+            StartCoroutine(MoveObject(moveVector, timeFeedback, () => UIManager.GetOnMovingToolCompleted?.Invoke()));
+
+            /*rt.DOMove(moveVector, timeFeedback).OnComplete(() =>
             {
                 UIManager.GetOnMovingToolCompleted?.Invoke();
-            });
+            });*/
+
             isActive = false;
         }
         else                                                //Sinon, il n'est pas à sa place initiale, il veut y retourner ! 
         {
             UIManager.GetOnMovingTool?.Invoke();
-            rt.DOMove(initialPos, timeFeedback).OnComplete(() =>
+
+            StartCoroutine(MoveObject(initialPos, timeFeedback, () => UIManager.GetOnMovingToolCompleted?.Invoke()));
+
+            /*rt.DOMove(initialPos, timeFeedback).OnComplete(() =>
             {
                 UIManager.GetOnMovingToolCompleted?.Invoke();
-            });
+            });*/
+
             isActive = true;
         }
 
+    }
+
+    IEnumerator MoveObject(Vector3 endPosition, float travelTime, Action callback)
+    {
+        float timeTraveled = 0;
+
+        Vector3 startPosition = rt.position;
+        Vector3 direction = endPosition - startPosition;
+
+        while(timeTraveled < travelTime)
+        {
+            timeTraveled += Time.deltaTime;
+
+            if(timeTraveled > travelTime)
+            {
+                timeTraveled = travelTime;
+            }
+
+            rt.position = startPosition + (direction * curve.Evaluate(timeTraveled / travelTime));
+
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        callback?.Invoke();
     }
 }
