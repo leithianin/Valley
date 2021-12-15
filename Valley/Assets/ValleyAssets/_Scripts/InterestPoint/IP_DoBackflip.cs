bc9a8d0d1b0100fb454fb7f6dcb8c66ed45cada6
TimotheHome <linkazer@gmail.com>
2021-12-15 11:11:30 +0100

    Summary for InterestPoint system

--- Valley/Assets/ValleyAssets/_Scripts/InterestPoint/IP_DoBackflip.cs
+++ Valley/Assets/ValleyAssets/_Scripts/InterestPoint/IP_DoBackflip.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class IP_DoBackflip : InterestPoint
 {
+    /// <summary>
+    /// The type of InterestPoint it is.
+    /// </summary>
     public override InterestPointType PointType => InterestPointType.ForHiker;
 
     [SerializeField] private VisitorAgentBehave currentVisitor;
 
+    /// <summary>
+    /// Check if a visitor is already using the InterestPoint.
+    /// </summary>
+    /// <returns>Return true if no visitor is using the InterestPoint.</returns>
     public override bool IsUsable()
     {
-        return true;// currentVisitor == null;
+        return currentVisitor == null;
     }
 
+    /// <summary>
+    /// When the interaction end, we remove the current visitor.
+    /// </summary>
+    /// <param name="visitor">The visitor that end the interaction.</param>
     protected override void OnEndInteraction(VisitorAgentBehave visitor)
     {
         currentVisitor = null;
     }
 
+    /// <summary>
+    /// When a visitor interact, we store it and start a coroutine to make an action.
+    /// </summary>
+    /// <param name="visitor"></param>
     protected override void OnVisitorInteract(VisitorAgentBehave visitor)
     {
         currentVisitor = visitor;
-        EndInteraction(currentVisitor);
-        //StartCoroutine(Backflip());
+        StartCoroutine(Backflip());
     }
 
     IEnumerator Backflip()
--- Valley/Assets/ValleyAssets/_Scripts/InterestPoint/InterestPoint.cs
+++ Valley/Assets/ValleyAssets/_Scripts/InterestPoint/InterestPoint.cs
@@ -4,24 +4,53 @@ using UnityEngine;
 
 public abstract class InterestPoint : MonoBehaviour
 {
+    // To see how to create an InterestPoint, check the IP_DoBackflip script.
+
+    // Satisfaction given when a visitor interact with the interest point.
     [SerializeField] private float satisfactionGiven;
 
+    /// <summary>
+    /// The type of InterestPoint it is. Must be override for every Interest Point.
+    /// </summary>
     public virtual InterestPointType PointType => InterestPointType.None;
-
+    
+    /// <summary>
+    /// The type of Landmark it is. Must be override for every Landmark.
+    /// </summary>
     public virtual LandMarkType LandmarkType => LandMarkType.None;
 
+    /// <summary>
+    /// Chec if the Interest Point can be used by a visitor.
+    /// </summary>
+    /// <returns>Return true if the visitor can interact with it.</returns>
     public abstract bool IsUsable();
 
+    /// <summary>
+    /// Do specific action when the visitor interact with the InterestPoint.
+    /// </summary>
+    /// <param name="visitor">The visitor thay interacted with the InterestPoint.</param>
     protected abstract void OnVisitorInteract(VisitorAgentBehave visitor);
 
+    /// <summary>
+    /// Do specific actions when the visitor interaction end.
+    /// </summary>
+    /// <param name="visitor">The visitor that end the interaction.</param>
     protected abstract void OnEndInteraction(VisitorAgentBehave visitor);
 
+    /// <summary>
+    /// Make a visitor interact with the InterestPoint.
+    /// </summary>
+    /// <param name="visitor">The visitor that is interacting.</param>
     public void MakeVisitorInteract(VisitorAgentBehave visitor)
     {
         visitor.AddSatisfaction(satisfactionGiven, true);
         OnVisitorInteract(visitor);
     }
 
+    /// <summary>
+    /// End the interaction with a visitor.
+    /// </summary>
+    /// <param name="visitor">The visitor that end the interaction.</param>
     public void EndInteraction(VisitorAgentBehave visitor)
     {
         OnEndInteraction(visitor);
