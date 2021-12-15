de41fc5e115401c0f28f8f12cd446bca9377b371
TimotheHome <linkazer@gmail.com>
2021-12-15 11:02:23 +0100

    Summary for Construction system

--- Valley/Assets/ValleyAssets/_Scripts/Construction/Construction.cs
+++ Valley/Assets/ValleyAssets/_Scripts/Construction/Construction.cs
@@ -6,45 +6,77 @@ using UnityEngine.Events;
 
 public abstract class Construction : MonoBehaviour
 {
+    // The cost of the contruction.
     [SerializeField] private int cost;
 
-    [SerializeField] private UnityEvent PlayOnPlace;
-    [SerializeField] private UnityEvent PlayOnDelete;
-    [SerializeField] private UnityEvent PlayOnSelect;
+    [SerializeField, Tooltip("Actions to play when the construction is placed.")] private UnityEvent PlayOnPlace;
+    [SerializeField, Tooltip("Actions to play when the construction is deleted.")] private UnityEvent PlayOnDelete;
+    [SerializeField, Tooltip("Actions to play when the construction is selected.")] private UnityEvent PlayOnSelect;
 
+    /// <summary>
+    /// Return the Tools used for this construction.
+    /// </summary>
+    /// <returns>The Tool linked to the construction.</returns>
     public abstract SelectedTools LinkedTool();
 
+    /// <summary>
+    /// Used to do specific action when a construction is placed.
+    /// </summary>
+    /// <param name="position">The position where the construction is placed.</param>
     protected abstract void OnPlaceObject(Vector3 position);
 
+    /// <summary>
+    /// Used to do specific action when a construction is removed.
+    /// </summary>
     protected abstract void OnRemoveObject();
 
+    /// <summary>
+    /// Used to do specific action when a construction is selected.
+    /// </summary>
     protected abstract void OnSelectObject();
 
+    /// <summary>
+    /// Get the cost of the construction.
+    /// </summary>
     public int GetCost => cost;
 
+    /// <summary>
+    /// Play the feedbacks and special actions when the object is placed.
+    /// </summary>
+    /// <param name="position">The position where the object is placed.</param>
     public void PlaceObject(Vector3 position)
     {
         PlayOnPlace?.Invoke();
         OnPlaceObject(position);
     }
 
+    /// <summary>
+    /// Play the feedbacks and special actions when the object is removed.
+    /// </summary>
     public void RemoveObject()
     {
         PlayOnDelete?.Invoke();
         OnRemoveObject();
     }
 
+    /// <summary>
+    /// Play the feedbacks and special actions when the object is removed.
+    /// </summary>
     public void SelectObject()
     {
         PlayOnSelect?.Invoke();
         OnSelectObject();
     }
 
+    //CODE REVIEW : Useless.
     public void SpawnObject(Vector3 position)
     {
 
     }
 
+    /// <summary>
+    /// Destroy the object.
+    /// </summary>
     public void DespawnObject()
     {
         Destroy(gameObject);
--- Valley/Assets/ValleyAssets/_Scripts/Construction/ConstructionPreview.cs
+++ Valley/Assets/ValleyAssets/_Scripts/Construction/ConstructionPreview.cs
@@ -6,29 +6,52 @@ using UnityEngine.AI;
 
 public abstract class ConstructionPreview : MonoBehaviour
 {
+    // The Construction linked to the preview.
     [SerializeField] protected Construction realConstruction;
 
+    // A list of all the object blocking the placement of the construction.
     protected List<GameObject> objectBlockingPose = new List<GameObject>();
 
+    // The senvitivity with which the preview will check the Navmesh.
     [SerializeField] protected float navMeshSensitivity = .5f;
 
+    // The mesh renderer of the preview.
     [SerializeField] private MeshRenderer mesh;
 
+    // The preview material when the player can place the object.
     [SerializeField] protected Material availableMaterial;
+    // The preview material when the player can't place the object.
     [SerializeField] protected Material unavailableMaterial;
 
-    [SerializeField] protected UnityEvent PlayOnAskToPlaceTrue;
-    [SerializeField] protected UnityEvent PlayOnAskToPlaceFalse;
-    [SerializeField] protected UnityEvent PlayOnAskToPlaceNoMoney;
+    [SerializeField, Tooltip("Feedbacks to play when the player succeed to place the object.")] protected UnityEvent PlayOnAskToPlaceTrue;
+    [SerializeField, Tooltip("Feedbacks to play when the player try to place the object without being able to.")] protected UnityEvent PlayOnAskToPlaceFalse;
+    [SerializeField, Tooltip("Feedbacks to play when the player hasn't enough ressources to place the construction.")] protected UnityEvent PlayOnAskToPlaceNoMoney;
 
     protected bool availabilityState = true;
 
+    /// <summary>
+    /// Getter for the Construction.
+    /// </summary>
     public Construction RealConstruction => realConstruction;
 
+    /// <summary>
+    /// Used to do specific action when we want to check if the construction can be placed.
+    /// </summary>
+    /// <param name="position">The position of the construction preview.</param>
+    /// <returns>Return true if the player can place the construction.</returns>
     protected abstract bool OnCanPlaceObject(Vector3 position);
 
+    /// <summary>
+    /// Used to do specific action when the player try to place the construction.
+    /// </summary>
+    /// <param name="position">The position where the player want to place the construction.</param>
     protected abstract void OnAskToPlace(Vector3 position);
 
+    /// <summary>
+    /// Called when the player try to place the construction.
+    /// </summary>
+    /// <param name="position">The position where the player want to place the construction.</param>
+    /// <returns>Return true if the player can place the construction.</returns>
     public bool AskToPlace(Vector3 position)
     {
         bool canPlace = CanPlaceObject(position);
@@ -50,6 +73,11 @@ public abstract class ConstructionPreview : MonoBehaviour
         return canPlace;
     }
 
+    /// <summary>
+    /// Called when we want to check if the construction can be placed at the specified position.
+    /// </summary>
+    /// <param name="position">The position to check.</param>
+    /// <returns>Return true if the player can place the construction.</returns>
     public bool CanPlaceObject(Vector3 position)
     {
         bool toReturn = true;
@@ -73,16 +101,26 @@ public abstract class ConstructionPreview : MonoBehaviour
         return toReturn && OnCanPlaceObject(position);
     }
 
+    /// <summary>
+    /// Spawn the construction preview.
+    /// </summary>
+    /// <param name="position">The position of the spawn.</param>
     public void SpawnObject(Vector3 position)
     {
 
     }
 
+    /// <summary>
+    /// Destroy the construction preview GameObject.
+    /// </summary>
     public void DespawnObject()
     {
         Destroy(gameObject);
     }
 
+    /// <summary>
+    /// Update the material of the mesh depending of the availability.
+    /// </summary>
     public void CheckAvailability()
     {
         if(CanPlaceObject(transform.position) != availabilityState)
